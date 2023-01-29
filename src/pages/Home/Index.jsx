19a701a6a5b10fa795727b61ffb770e86c417344
thais de rodez <thaisderodez@gmail.com>
2023-01-29 14:32:38 +0100

    feat(home): banner and cards on home desktop

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,7 +1,19 @@
 import './../../styles/Pages/Home.css';
+import Banner from '../../components/Banner';
+import Card from '../../components/Card';
+import accomodations from '../../datas/accomodations.json';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+      <section className="cards-section">
+        {accomodations.map(({ id, title, cover }) => (
+          <Card key={id} title={title} cover={cover} />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default Home;
