bda66db6496c68180b8453113c7060f16e960621
thais de rodez <thaisderodez@gmail.com>
2023-01-29 14:32:05 +0100

    feat(banner): banner componant

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import './../styles/Components/Banner.css';
+
+function Banner() {
+  return (
+    <section className="banner-section">
+      <p className="banner-text">Chez vous, partout et ailleurs</p>
+    </section>
+  );
+}
+
+export default Banner;
