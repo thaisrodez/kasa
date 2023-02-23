ca1ff9b3fa4df5b36472c807af85140fc89f283a
thais de rodez <thaisderodez@gmail.com>
2023-02-23 15:12:14 +0100

    feat(about): about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,15 @@
 import Banner from '../../components/Banner';
 import aboutBanner from '../../assets/about-banner.png';
+import abouts from '../../datas/about.json';
+import Collapse from '../../components/Collapse';
 
 function About() {
   return (
     <div>
       <Banner url={aboutBanner} />
+      {abouts.map(({ id, title, text }) => (
+        <Collapse key={id} title={title} text={text} />
+      ))}
     </div>
   );
 }
