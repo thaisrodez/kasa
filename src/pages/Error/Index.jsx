33e7e31ea2d626c27f50a7b19fad31e13cf2df59
thais de rodez <thaisderodez@gmail.com>
2023-02-23 15:40:00 +0100

    feat(error): design error page

--- src/pages/Error/Index.jsx
+++ src/pages/Error/Index.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import '../../styles/Pages/Error.css';
+
 function ErrorPage() {
-  return <div>Error</div>;
+  return (
+    <div className="error">
+      <h1 className="error-title">404</h1>
+      <p className="error-text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error-link">
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  );
 }
 
 export default ErrorPage;
