1e7970cd5ba6a0b954aa0f11d639c8fe22022948
thais de rodez <thaisderodez@gmail.com>
2023-02-23 16:40:21 +0100

    feat(accomodation): start accomodation page

--- src/pages/Accomodation/Index.jsx
+++ src/pages/Accomodation/Index.jsx
@@ -1,7 +1,18 @@
 import { useParams } from 'react-router-dom';
+import Carrousel from '../../components/Carrousel';
+import accomodations from '../../datas/accomodations.json';
 
 function Accomodation() {
   const { accomodationId } = useParams();
-  return <div>Accomodation</div>;
+
+  const accomodation = () => {
+    return accomodations.find(({ id }) => id === accomodationId);
+  };
+
+  return (
+    <div>
+      <Carrousel pictures={accomodation().pictures} />
+    </div>
+  );
 }
 export default Accomodation;
