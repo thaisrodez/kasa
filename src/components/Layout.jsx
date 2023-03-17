b35d32679d218d1941d53b2e47130f6220fb4a8e
thais de rodez <thaisderodez@gmail.com>
2023-03-17 15:24:08 +0100

    fix(eslint): reinstall linter

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 
-function Layout({ children }) {
+function Layout() {
   return (
     <div>
       <main className="kasa-main">
